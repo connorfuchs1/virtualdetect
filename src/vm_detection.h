5d6dfd78bdd7674565c82cddf24ceb1e46f88bbc
connor <connor.fuchs@twosixtech.com>
2024-11-04 15:17:08 -0500

    function name change in .h file

--- src/vm_detection.h
+++ src/vm_detection.h
@@ -17,7 +17,7 @@ int runIndividualTest(OS_TYPE OS, const std::string& testName);
 
 //individual tests
 bool checkIODevices(OS_TYPE OS);
-bool checkCPUFeatures(OS_TYPE OS);
+bool checkHypervisorBit(OS_TYPE OS);
 bool checkVendorID(OS_TYPE OS);
 
 // Add additional test function declarations here...
